dc4487f9916fa7db7de67831d56dc296c06ed0f5
cipster <ciprian@rain-software.com>
2019-01-16 18:46:49 +0200

    full application running via docker-compose

--- admin-service/src/main/java/nl/globalorange/compliancewise/administration/AdminServiceApplication.java
+++ admin-service/src/main/java/nl/globalorange/compliancewise/administration/AdminServiceApplication.java
@@ -2,8 +2,12 @@ package nl.globalorange.compliancewise.administration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableJpaAuditing
 public class AdminServiceApplication {
 
     public static void main(String[] args) {
